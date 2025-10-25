53f665ca52f17eb3ae909eaa13e89e7fb04f4b4a
H. Vetinari <h.vetinari@gmx.com>
2025-10-25 12:10:49 +0200

    add z3-solver python bindings

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -2,10 +2,14 @@
 
 set -xe
 
-mkdir -p build_lib && cd build_lib
+mkdir -p build_py && cd build_py
 
 cmake .. -G "Ninja" \
     ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PYTHON_PKG_DIR=${SP_DIR} \
+    -DPython3_EXECUTABLE=${PYTHON} \
+    -DZ3_BUILD_PYTHON_BINDINGS=TRUE \
+    -DZ3_INSTALL_PYTHON_BINDINGS=TRUE \
     -DZ3_BUILD_LIBZ3_SHARED=TRUE \
     -DZ3_INCLUDE_GIT_DESCRIBE=FALSE \
     -DZ3_INCLUDE_GIT_HASH=FALSE \
