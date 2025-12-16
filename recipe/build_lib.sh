4d98790580038be54f83e64643b7842f1365a2c4
H. Vetinari <h.vetinari@gmx.com>
2025-12-16 21:43:14 +1100

    backport upstream PR for split builds

--- recipe/build_lib.sh
+++ recipe/build_lib.sh
@@ -6,6 +6,7 @@ mkdir -p build_lib && cd build_lib
 
 cmake .. -G "Ninja" \
     ${CMAKE_ARGS} \
+    -DZ3_BUILD_LIBZ3_CORE=ON \
     -DZ3_BUILD_LIBZ3_SHARED=TRUE \
     -DZ3_INCLUDE_GIT_DESCRIBE=FALSE \
     -DZ3_INCLUDE_GIT_HASH=FALSE \
