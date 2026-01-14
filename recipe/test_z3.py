af3fa626bdb01ba6e92e687bf724381af7cc40b9
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-01-13 19:43:59 -0600

    more test comment

--- recipe/test_z3.py
+++ recipe/test_z3.py
@@ -5,7 +5,9 @@ import z3
 PKG_VERSION = os.environ["PKG_VERSION"]
 PKG_NAME = os.environ["PKG_NAME"]
 
+
 def test_version():
+    """Verify various versions agree (if are not the same): seems to be a trailing digit."""
     module_version = importlib.metadata.version(PKG_NAME)
     libz3_version = z3.get_version_string()
     print(f"{PKG_NAME} module version:", module_version)
@@ -13,9 +15,11 @@ def test_version():
     assert module_version.startswith(f"{PKG_VERSION}.")
     assert libz3_version == PKG_VERSION
 
+
 def test_z3():
+    """Verify simple smoke test, adapted from upstream example."""
     s = z3.Solver()
-    x, y = z3.Real('x'), z3.Real('y')
+    x, y = z3.Real("x"), z3.Real("y")
     s.add(x + y > 5, x > 1, y > 1)
     print(s.check())
     print(s.model())
